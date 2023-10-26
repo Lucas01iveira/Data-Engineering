c0d60ec35473cce727d30fb57addc610b411d304
Lucas de Paula Oliveira <loliveirab1030@gmail.com>
2023-10-26 19:32:05 -0300

    otimizacao jogo forca

--- Level 1 - Data Engineering/Formation - Learning Python with OOP/Python - Beginning with the language/forca.py
+++ Level 1 - Data Engineering/Formation - Learning Python with OOP/Python - Beginning with the language/forca.py
@@ -1,92 +1,183 @@
 from time import sleep
 import string
-
-# função auxiliar responsável por apresentar a interface do jogo
-def loop_printa_lista(texto):
-    for index in range(len(texto)):
-            #if (index == len(texto)-1):
-            #    print(texto[index])
-            #else:
-            #    print(texto[index]+' ', end='')
-            print(texto[index],end=' ')
+import random as r 
 
 def inicializar_jogo():
-    # Cabeçalho de apresentação do mini-game
+
     sleep(1)
-    print('-'*35)
-    print('Bem vindo(a) ao jogo da forca!')
-    print('-'*35)
-    print()
+    apresenta_cabecalho()
 
-    # definição da palavra secreta do jogo 
-    palavra_secreta = 'banana'.lower()
-    
-    # defino uma lista para nos auxiliar na emissão e atualização da interface do jogo
-    #auxiliar_interface_jogo = []
-    #for letra in palavra_secreta:
-    #    auxiliar_interface_jogo.append('_')
+    arquivo_fonte_palavras = r'Level 1 - Data Engineering\Formation - Learning Python with OOP\Python - Beginning with the language\palavras.txt'
+    palavra_secreta = seleciona_palavra_secreta(arquivo_fonte_palavras)
+    auxiliar_interface_jogo = ['_' for letra in palavra_secreta]
 
-    # outra maneira de fazer:
-    auxiliar_interface_jogo = ['_' for letra in palavra_secreta] # funcionalidade de "list comprehension"
+    enforcou = False
+    acertou = False 
 
-    # outra maneira de fazer:
-    #auxiliar_interface_jogo = ['_']*len(palavra_secreta)
+    total_tentativas = 7
+    erros = 0 
 
-    # definição de duas variáveis auxiliares para controlar o andamento do fluxo do jogo
-    enforcou = False # armazena a informação se o jogador foi enforcado ou não (início False: o jogador não começa enforcado)
-    acertou = False # armazena a informação se o jogador ganhou ou não (início False: o jogador não começa já vencendo)
-
-    total_tentativas = len(palavra_secreta) # defino um número máximo de tentativas para o jogador com base na quantidade de caracteres
-    erros = 0 # variável auxiliar para contar a quantidade de erros do jogador
-
-    # outra maneira de escrever a lógica abaixo: while (enforcou == False) and (acertou == False):
     while (not enforcou) and (not acertou):
         
         loop_printa_lista(auxiliar_interface_jogo)
         print(end=' ')
-        # obtém uma letra inserida pelo usuário
-        chute = input('Digite uma letra SEM ACENTOS: ').strip().lower()
 
-        # efetua uma validação do chute inserido a fim de evitar caracteres especiais
-        if (chute in string.punctuation) or (chute in ('~~','^^')):
-            while chute in string.punctuation:
-                chute = input('Caracteres especiais não são permitidos. Por favor, digite novamente: ').strip().lower()
-        
-        # verifico se a letra chutada já foi chutada antes
-        if (chute in auxiliar_interface_jogo): # ou seja, se a letra chutada já foi atualizada na variável auxiliar
-            while chute in auxiliar_interface_jogo:
-                chute = input('Esse caractere já foi identificado. Por favor, insira uma nova letra: ').strip().lower()
-        
-        # verifico se a letra chutada está presente na palavra secreta
+        chute = valida_chute_efetuado(input('Digite uma letra SEM ACENTOS: ').strip().lower(), auxiliar_interface_jogo)
+
         if (chute not in palavra_secreta):
             erros += 1
-            print('Você errou! Restam apenas {} tentativas.'.format(total_tentativas-erros))
+            sinaliza_tentativas_restantes(total_tentativas, erros)            
         else:
-            for index in range(len(palavra_secreta)):
-                if (chute == palavra_secreta[index]):
-                    auxiliar_interface_jogo[index] = chute
-        
-        # condições de finalização do loop!
-        # 1) verifico se a minha lista auxiliar já está completamente preenchida
-        if ('_' not in auxiliar_interface_jogo): # se não houverem mais '_' significa que a pessoa preencheu todas as letras da palavra oculta
-            print('-'*55)
-            loop_printa_lista(auxiliar_interface_jogo)
-            print()
-            print('Parabéns! Você venceu o jogo da forca.')
-            acertou = True
-        
-        # 2) verifico se o jogador já esgotou todas as suas tentativas
-        elif (erros == total_tentativas):
-            print('-'*45)
-            print('Que pena! Você perdeu.\nObrigado por jogar!')
-            enforcou = True
+            contabiliza_acerto(chute ,auxiliar_interface_jogo, palavra_secreta)
+            
+        acertou, enforcou = verifica_encerramento(auxiliar_interface_jogo, acertou, enforcou, total_tentativas, erros, palavra_secreta)
 
         print('-'*45)
         print()
 
 
-# A variável __name__ é definida sempre que um arquivo python é executado;
-# se esse arquivo é executado diretamente pelo prompt de comando (como um arquivo python "principal),
-# então queremos que o código dentro dele seja executado automaticamente. Daí a relevância do if abaixo
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+def imprime_mensagem_vencedor():
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
+def imprime_mensagem_perdedor(palavra_secreta):
+    print("Puxa, você foi enforcado!")
+    print("A palavra era {}".format(palavra_secreta))
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
+def loop_printa_lista(texto):
+    for index in range(len(texto)):
+        print(texto[index],end=' ')
+
+def apresenta_cabecalho():
+    print('-'*35)
+    print('Bem vindo(a) ao jogo da forca!')
+    print('-'*35)
+    print()
+
+def seleciona_palavra_secreta(arquivo_fonte):
+    lista_palavras = []
+
+    with open(arquivo_fonte, 'r') as palavras:
+        for palavra in palavras:
+            lista_palavras.append(palavra.strip())
+
+    return r.choice(lista_palavras).lower()
+
+def valida_chute_efetuado(letra, lista_auxiliar_jogo):
+    if (letra in string.punctuation) or (letra in ('~~','^^')):
+            while letra in string.punctuation:
+                letra = input('Caracteres especiais não são permitidos. Por favor, digite novamente: ').strip().lower()
+    
+    elif(letra in lista_auxiliar_jogo): 
+            while letra in lista_auxiliar_jogo:
+                letra = input('Esse caractere já foi identificado. Por favor, insira uma nova letra: ').strip().lower()
+
+    return letra
+
+def sinaliza_tentativas_restantes(total_tentativas, erros):
+    print('Você errou! Restam apenas {} tentativas.'.format(total_tentativas-erros))
+    desenha_forca(erros)
+
+def contabiliza_acerto(letra, lista_auxiliar, palavra):
+    for index in range(len(palavra)):
+                if (letra == palavra[index]):
+                    lista_auxiliar[index] = letra
+
+def verifica_encerramento(lista_auxiliar, bool_sucesso, bool_fracasso, total_tentativas, erros, palavra):
+    if ('_' not in lista_auxiliar): 
+        #print('-'*55)
+        #loop_printa_lista(lista_auxiliar)
+        #print()
+        #print('Parabéns! Você venceu o jogo da forca.')
+        print('-'*55)
+        loop_printa_lista(lista_auxiliar)
+        print()
+        imprime_mensagem_vencedor()
+        bool_sucesso = True
+        
+    elif (erros == total_tentativas):
+        #print('-'*45)
+        #print('Que pena! Você perdeu.\nA palavra secreta era {}'.format(palavra))
+        #print('Obrigado por jogar!')
+        imprime_mensagem_perdedor(palavra)
+        bool_fracasso = True
+    
+    return bool_sucesso, bool_fracasso
+
 if (__name__ == '__main__'):
     inicializar_jogo()

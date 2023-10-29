8c6178d49c77f11d8ea0078bdd92328bebf05e92
Lucas de Paula Oliveira <loliveirab1030@gmail.com>
2023-10-29 19:02:22 -0300

    definição de getters e setters

--- Level 1 - Data Engineering/Formation - Learning Python with OOP/Python - Understanding Object Orientation/conta.py
+++ Level 1 - Data Engineering/Formation - Learning Python with OOP/Python - Understanding Object Orientation/conta.py
@@ -6,33 +6,61 @@ class Conta:
 
     # função construtora
     def __init__(self, numero, titular, saldo, limite):
+        # sempre é possível definir parâmetros opcionais, implementando a sintaxe def __init__ (self, .. , <param_opcional> = <valor_default>)
+        # desse modo, caso o parâmetro não seja informado no momento da definição do objeto, o valor default será atribuído.
         
         print('Construindo o objeto da classe em.. {}'.format(self))
 
         # atribuição das características da classe (i.e., dos objetos que serão criados)
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
-        # uma vez que eu digito "self.<atributo>", já estou informado para o python que o atributo
-        # em questão pertence AO OBJETO que acabou de ser criado.
+        # uma vez que eu digito "self.<atributo>", já estou informado para o python que o atributo em questão pertence AO OBJETO que acabou de ser criado.
+
+        # sempre que o prefixo "__" é incluído antes da chamada do atributo, estamos impondo que esse atributo seja do tipo "privado"; ou seja, mesmo que o programador tente acessá-los a manipulá-los diretamente com auxílio da sintaxe <ref_objeto>._<Classe>__<atributo>, esse padrão de nomenclatura já torna explícito o fato de que esse atributo deve ser alterado / acessado somente através dos métodos da classe
+
     
     # função relativa aos objetos dessa classe (método): consulta extrato
     def extrato(self):
-        print('A conta do titular {} possui {} de saldo'.format(self.titular, self.saldo)) 
+        print('A conta do titular {} possui {} de saldo'.format(self.__titular, self.__saldo)) 
 
     # função relativa aos objetos dessa classe (método): deposita dinheiro
     def deposita(self, valor):
-        if (self.saldo + valor <= self.limite):
-            self.saldo += valor
+        if (self.__saldo + valor <= self.__limite):
+            self.__saldo += valor
         else:
-            print('Você não pode depositar a quantia de {}, pois o seu saldo final irá ultrapassar o limite máximo permitido de {}'.format(valor, self.limite))
+            print('Você não pode depositar a quantia de {}, pois o seu saldo final irá ultrapassar o limite máximo permitido de {}'.format(valor, self.__limite))
         
     # função relativa aos objetos dessa classe (método): saca dinheiro
     def saca(self, valor):
-        if (self.saldo - valor >= 0):
-            self.saldo -= valor
+        if (self.__saldo - valor >= 0):
+            self.__saldo -= valor
         else:
             print('Você não pode sacar a quantia de {}, pois o seu saldo final ficará negativo'.format(valor))
     
+    # mfunção relativa aos objetos dessa classe (método): transfere dinheiro
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    # método "getter" para consultar o número da conta
+    def get_numero(self):
+        return self.__numero
+
+    # método "getter" para consultar o limite da conta 
+    def get_limite(self):
+        return self.__limite
+    
+    # método "getter" para consultar o saldo da conta
+    def get_saldo(self):
+        return self.__saldo
+
+    # método "getter" para consultar o titular da conta
+    def get_titular(self):
+        return self.__titular
+    
+    # método "setter" para ajustar o limite da conta
+    def set_limite(self, novo_limite):
+        self.__limite = novo_limite

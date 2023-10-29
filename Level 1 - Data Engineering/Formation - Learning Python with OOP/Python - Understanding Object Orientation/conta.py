015b52754d1e6b1a528321ae3d1890e715baca54
Lucas de Paula Oliveira <loliveirab1030@gmail.com>
2023-10-29 16:06:57 -0300

    Finalização da classe Conta

--- Level 1 - Data Engineering/Formation - Learning Python with OOP/Python - Understanding Object Orientation/conta.py
+++ Level 1 - Data Engineering/Formation - Learning Python with OOP/Python - Understanding Object Orientation/conta.py
@@ -4,11 +4,12 @@ class Conta:
     # self = endereçamento do objeto na memória do computador;
     #    é criado automaticamente pelo python quando a classe é instanciada em alguma variável
 
-    # __init__ = função construtora
+    # função construtora
     def __init__(self, numero, titular, saldo, limite):
         
+        print('Construindo o objeto da classe em.. {}'.format(self))
+
         # atribuição das características da classe (i.e., dos objetos que serão criados)
-        
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
@@ -16,3 +17,22 @@ class Conta:
 
         # uma vez que eu digito "self.<atributo>", já estou informado para o python que o atributo
         # em questão pertence AO OBJETO que acabou de ser criado.
+    
+    # função relativa aos objetos dessa classe (método): consulta extrato
+    def extrato(self):
+        print('A conta do titular {} possui {} de saldo'.format(self.titular, self.saldo)) 
+
+    # função relativa aos objetos dessa classe (método): deposita dinheiro
+    def deposita(self, valor):
+        if (self.saldo + valor <= self.limite):
+            self.saldo += valor
+        else:
+            print('Você não pode depositar a quantia de {}, pois o seu saldo final irá ultrapassar o limite máximo permitido de {}'.format(valor, self.limite))
+        
+    # função relativa aos objetos dessa classe (método): saca dinheiro
+    def saca(self, valor):
+        if (self.saldo - valor >= 0):
+            self.saldo -= valor
+        else:
+            print('Você não pode sacar a quantia de {}, pois o seu saldo final ficará negativo'.format(valor))
+    

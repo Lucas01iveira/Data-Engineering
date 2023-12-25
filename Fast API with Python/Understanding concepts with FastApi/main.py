79fea9a7e55632a8a6a55a4d4242b4420f2faded
Lucas de Paula Oliveira <loliveirab1030@gmail.com>
2023-12-25 19:04:20 -0300

    entendendo o conceito de ingestão de dependências no FastAPI

--- Fast API with Python/Understanding concepts with FastApi/main.py
+++ Fast API with Python/Understanding concepts with FastApi/main.py
@@ -1,4 +1,4 @@
-from typing import List, Optional # classes de typing hints
+from typing import List, Optional, Any # classes de typing hints
 from fastapi import FastAPI # classe de definição do aplicativo
 from fastapi import HTTPException # classe para retorno do objeto HttpException
 from fastapi import status # constante com códigos de de resposta http (para cada tipo de log) 
@@ -6,8 +6,11 @@ from fastapi import Response # classe para retorno da resposta 204 (no content /
 from fastapi import Path # classe auxiliar de validação do parâmetro passado no endpoint get (path)
 from fastapi import Query # classe auxiliar de validação do parâmetro passado na query do endpoint get
 from fastapi import Header # classe auxiliar de validação do header da requisição get
+from fastapi import Depends # classe auxiliar para definição de dependências
 from fastapi.responses import JSONResponse # classe para retorno da resposta 204 (ainda em dev)
 from models import Curso # classe curso (referente ao projeto da nossa aplicação)
+
+from time import sleep
 
 # acesso da documentação no navegador:
 # http://localhost:<numero_da_porta>/docs
@@ -33,12 +36,36 @@ cursos = {
     }
 }
 
+def conecta_database():
+    print('Conectando database', end='')
+    for _ in range(3):
+        if _ == 2:
+            print('.')
+            sleep(1)
+        else:
+            print('.', end='')
+            sleep(1)
+    print('Desligando database', end='')
+    for _ in range(3):
+        if _ == 2:
+            print('.')
+            sleep(1)
+        else:
+            print('.', end='')
+            sleep(1)
+    
+
+# método de consulta (GET)
 @app.get('/cursos') # definindo o endpoint (para método get)
-async def get_cursos():
+async def get_cursos(db: Any = Depends(conecta_database)):
     return cursos
 
+# método de consulta (GET)
 @app.get('/cursos/{curso_id}')
-async def get_cursos(curso_id: int = Path(default=None, description= 'O Id deve estar entre 0 e 3', gt=0, lt=3)): 
+async def get_cursos(
+    curso_id: int = Path(default=None, description= 'O Id deve estar entre 0 e 3', gt=0, lt=3)
+    ,db: Any = Depends(conecta_database)
+    ): 
     
     # apenas por indicar o typing hint da variável em utilização o próprio fastApi já tenta fazer a conversão automaticamente (visto que tudo que entra pelo usuário é string) e, se não for possível, uma mensagem de erro é retornada.
 
@@ -50,9 +77,13 @@ async def get_cursos(curso_id: int = Path(default=None, description= 'O Id deve 
             status_code= status.HTTP_404_NOT_FOUND
             ,details= 'Curso não encontrado')
 
+# método de criação/geração de dados (POST)
 # por padrão, o fastAPI retorna status 200 (mas para objetos criados/postados o correto seria 201), então temos que especificar manualmente dentro da definição do endpoint
 @app.post('/cursos', status_code= status.HTTP_201_CREATED)  
-async def post_curso(curso: Curso):
+async def post_curso(
+    curso: Curso
+    ,db: Any = Depends(conecta_database)
+    ):
     next_id = len(cursos)+1
 
     del curso.id # deletando o atributo id (optional // não queremos emití-lo nas consultas)
@@ -60,8 +91,12 @@ async def post_curso(curso: Curso):
     cursos[next_id] = curso
     return curso
 
+# método de atualização (PUT)
 @app.put('/cursos/{curso_id}')
-async def put_curso(curso_id: int, curso: Curso):
+async def put_curso(
+    curso_id: int
+    , curso: Curso, db: Any = Depends(conecta_database)
+    ):
     if curso_id not in cursos: 
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= 'O curso que você está tentando atualizar não foi encontrado no database')
     else:
@@ -69,8 +104,12 @@ async def put_curso(curso_id: int, curso: Curso):
         cursos[curso_id].update(curso)
         return curso
 
+# método para exclusão de dados (DELETE)
 @app.delete('/cursos/{curso_id}')
-async def delete_curso(curso_id: int):
+async def delete_curso(
+    curso_id: int
+    ,db: Any = Depends(conecta_database)
+    ):
     if curso_id not in cursos:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND
@@ -80,6 +119,8 @@ async def delete_curso(curso_id: int):
         del cursos[curso_id]
         #return JSONResponse(status_code=status.HTTP_204_NO_CONTENT) # por enquanto o json response não está 100% implementado (Sem bugs)
         return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+# -- x -- -- x -- -- x -- -- x -- -- x -- -- x -- -- x -- -- x -- -- x -- -- x -- -- x -- -- x -- -- x ---- x --
 
 # método extra apenas para entendimento das query parameters
 @app.get('/calculadora')

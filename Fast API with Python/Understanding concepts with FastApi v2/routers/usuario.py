a69429c35fa08943980d10655e89c8be5349153e
Lucas de Paula Oliveira <loliveirab1030@gmail.com>
2023-12-27 20:08:26 -0300

    melhorando a organização do código com routers

--- Fast API with Python/Understanding concepts with FastApi v2/routers/usuario.py
+++ Fast API with Python/Understanding concepts with FastApi v2/routers/usuario.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get('/api/v1/usuarios')
+@router.get('/api/v1/usuarios') # router de efetuação do comando 'get'
 async def get_cursos():
     return {'info':'todos os usuarios'}

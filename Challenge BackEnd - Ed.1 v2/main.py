d7e637afe9e21cd3fddbb28d3b47248a9eab012a
Lucas de Paula Oliveira <loliveirab1030@gmail.com>
2024-01-19 20:01:58 -0300

    atualizando documentacao do projeto

--- Challenge BackEnd - Ed.1 v2/main.py
+++ Challenge BackEnd - Ed.1 v2/main.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI
 
 from routes.routes import router_obj
 
-app = FastAPI()
+app = FastAPI(
+    title='Armazenamento de Url\'s'
+    ,description='API designada a controlar a movimentação de atualizações na tabela cbe.EnderecosVideos do banco de dados ProjetosFrontEnd no servidor local. A proposta do projeto é realizar uma integração entre a área de back-end e a área de front-end de modo que os endereços armazenados nesse ambiente sejam posteriormente utilizados para construção de um painel interativo pela equipe de desenvolvimento web.'
+    ,docs_url='/documentacao'
+    ,version='1.0.0'
+    ,openapi_url='/armazenamentourls.json'
+)
 app.include_router(router=router_obj, tags=['videos'])
 
 if __name__ == '__main__':

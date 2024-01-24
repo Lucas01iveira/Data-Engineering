a19afd3c7d005faefd9cabfa26e66599ec84379b
Lucas de Paula Oliveira <loliveirab1030@gmail.com>
2024-01-24 20:36:50 -0300

    atualizacao docs: get, post, put, delete

--- Challenge BackEnd - Ed.1 v2/routes/routes.py
+++ Challenge BackEnd - Ed.1 v2/routes/routes.py
@@ -15,10 +15,10 @@ router_obj = APIRouter()
 
 @router_obj.get(
         path= '/api/v2/teste'
-        ,summary= 'Testando o aplicativo'
-        ,description= 'Endpoint desenvolvido para teste de conexão / funcionamento da aplicação dentro do servidor alocado'
+        ,summary= 'Testando o aplicativo.'
+        ,description= 'Endpoint desenvolvido para teste de conexão / funcionamento da aplicação dentro do servidor alocado.'
         ,tags= ['Introducao']
-        ,response_description='O retorno do endpoint corresponde simplesmente a um json informativo (não interfere no desenvolvimento/funcionamento dos demais endpoints da aplicação)'
+        ,response_description='O retorno do endpoint corresponde simplesmente a um JSON informativo (não interfere no desenvolvimento/funcionamento dos demais endpoints da aplicação).'
         ,status_code=status.HTTP_200_OK
 )
 async def api_teste():
@@ -26,7 +26,7 @@ async def api_teste():
 
 @router_obj.get(
         path= '/api/v2/videos'
-        ,summary= 'Leitura da base de endereços'
+        ,summary= 'Leitura da base de endereços.'
         ,description= 'Retorna uma consulta de seleção de todas as informações de endereços cadastradas/inseridas na tabela cbe.EnderecosVideos, no banco ProjetosFrontEnd.'
         ,response_description='''O retorno do endpoint corresponde a uma lista de todos os endereços de vídeos presentes na tabela correspondente do banco de dados. Essa informação é repassada ao usuário em formato JSON. 
         
@@ -49,14 +49,14 @@ async def get_videos(
 
 @router_obj.get(
         path='/api/v2/videos/{video_id}'
-        ,summary='Extração de informações de um vídeo específico'
-        ,description='Efetua uma consulta de seleção das informações de um vídeo em específico, identificado pelo Id. Vale destacar que o Id deve ser um número inteiro positivo.'
-        ,response_description='''O retorno do endpoint corresponde ao endereço do vídeo (e suas informações complementares), dentro da base de dados, correspondente ao id informado pelo usuário. 
+        ,summary='Leitura da base de endereços.'
+        ,description='''Efetua uma consulta de seleção das informações de um vídeo em específico, identificado pelo Id informado. Vale destacar que o Id deve ser um número inteiro positivo.'''
+        ,response_description='''O retorno do endpoint corresponde ao endereço do vídeo (e suas informações complementares) cujo id é igual ao número 'video_id'. 
         
         Códigos de resposta possíveis: 
             - 200: O Id informado está mapeado na base de dados e as informações são retornadas;
-            - 404: O Id informado não está mapeado na base de dados e não informações a serem apresentadas;
-            - 422: O Id informado pelo usuário no endpoint não satisfaz os requisitos de validação.
+            - 404: O Id informado não está mapeado na base de dados e não há informações a serem apresentadas;
+            - 422: O Id informado no endpoint não satisfaz aos requisitos de validação.
         '''
         ,tags=['CRUD']
         #,response_model= Video
@@ -73,14 +73,14 @@ async def get_video(
     if len(df) == 0:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail='Não existe nenhum vídeo com o id informado'
+            detail='Não existe nenhum vídeo com o id informado.'
         )
     else:
         return df.transpose().to_dict()
     
 @router_obj.post(
         path='/api/v2/videos'
-        ,summary='Criação de novo endereço na base'
+        ,summary='Criação de novo endereço na base.'
         ,description='''O método post, correspondente a esse endpoint da API, é responsável por gerar uma nova linha na tabela cbe.EnderecosVideos contendo as 3 informações principais do projeto: Titulo, Descricao e URL. Essas informações devem ser enviadas no body da requisição e, com exceção do campo de descrição, todas as demais são obrigatórias e devem satisfazer às condições de validação. Não é necessário informar o Id do endereço que estará sendo parametrizado pois a tabela no SQL Server já foi configurada para gerar um novo Id a cada insert.
         
         Condições de validação:
@@ -89,8 +89,8 @@ async def get_video(
             - "Url" deve ter 'projetosfrontend.com' como servidor de hospedagem do aplicativo final web (exemplo: https://www.projetosfrontend.com.br/teste).
         '''
         ,response_description='''Códigos de resposta da chamada do endpoint:
-            - 201: O body da requisição foi validado corretamente e o novo cadastro foi inserido no banco
-            - 422: O body da requisição informado pelo usuário não satisfaz os requisitos de validação.
+            - 201: O body da requisição foi validado corretamente e o novo cadastro foi inserido no banco;
+            - 422: O body da requisição não satisfaz aos requisitos de validação.
         '''
         ,tags=['CRUD']
 )
@@ -116,7 +116,7 @@ async def post_video(
 
 @router_obj.put(
         path='/api/v2/videos/{video_id}'
-        ,summary='Atualiza informações do Id especificado'
+        ,summary='Atualização de informação na base.'
         ,description='''Atualiza, no registro com id igual a 'video_id' (passado pelo usuário), os campos "Titulo", "Descricao" e "Url" com os valores enviados pelo usuário no body da requisição. As informações no body seguem as mesmas restrições e obrigatoriedades do endpoint de post.
 
         Condições de validação:
@@ -126,7 +126,7 @@ async def post_video(
         '''
         ,response_description='''Códigos de resposta:
             - 404: O Id informado não está mapeado na base de dados e não informações a serem apresentadas;
-            - 422: O Id informado pelo usuário no endpoint não satisfaz os requisitos de validação.
+            - 422: O Id informado no endpoint não satisfaz aos requisitos de validação.
         
         Em caso de sucesso, a API irá retornar um JSON indicando as novas informações corretamente atualizadas para o Id especificado.
         '''
@@ -166,7 +166,19 @@ async def put_video(
         df.set_index('id', inplace=True)
         return df.transpose().to_dict()
 
-@router_obj.delete('/api/v2/videos/{video_id}')
+@router_obj.delete(
+        path='/api/v2/videos/{video_id}'
+        ,summary='Exclusão de registro na base.'
+        ,description='''
+        Deleta, da tabela cbe.EnderecosVideos, o registro com id igual a 'video_id'. O parâmetro em questão segue a mesma restrição dos endpoints get e put (deve ser um inteiro positivo).
+        '''
+        ,response_description='''Códigos de resposta:
+            - 201: O registro foi excluído corretamente;
+            - 404: O Id informado não está mapeado na base de dados e não informações a serem apresentadas;
+            - 422: O Id informado no endpoint não satisfaz aos requisitos de validação.
+        '''
+        ,tags=['CRUD']
+)
 async def delete_video(
     video_id: int = Path(default=None, gt=0, description='O id do video deve ser um inteiro maior que 0')
     ,conn: int = Depends(connect_to_sql_server_db)

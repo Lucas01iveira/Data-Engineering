f5c5fc2c3ec3ec03d6fef659abd0af3f37323991
Lucas de Paula Oliveira <loliveirab1030@gmail.com>
2024-01-25 16:04:47 -0300

    populando tabela de categorias

---
+++ Challenge BackEnd - Ed.1 v2/Ambiente-SQL/InsertCategoriasExemplo.sql
@@ -0,0 +1,16 @@
+insert into cbe.Categoria
+select 'Livre', '008000' -- Green (base hexadecimal // css)
+
+insert into cbe.Categoria
+select 'Ação', 'FF4500' -- OrangeRed
+
+insert into cbe.Categoria
+select 'Comédia', 'FFD700' -- Gold
+
+insert into cbe.Categoria
+select 'Aventura', '00FFFF' -- Aqua
+
+insert into cbe.Categoria
+select 'Suspense', '8B0000' -- DarkRed
+
+

a46924e1154489186edef72240ce6465f2e3d00d
Lucas de Paula Oliveira <loliveirab1030@gmail.com>
2024-01-25 16:09:45 -0300

    atualizacao - script geracao coluna categoria

--- Challenge BackEnd - Ed.1 v2/Ambiente-SQL/GeraColunaCategoria.sql
+++ Challenge BackEnd - Ed.1 v2/Ambiente-SQL/GeraColunaCategoria.sql
@@ -1,7 +1,7 @@
 alter table cbe.EnderecosVideos
-	add categoriaId int
+	add CategoriaId int
 go
 
 -- Seguindo regra de negócio: caso o vídeo não possua categoria, então incluímos a referência ao Id = 1 (livre)
 update cbe.EnderecosVideos
-	set categoriaId = 1 where categoriaId is null
+	set CategoriaId = 1 where CategoriaId is null

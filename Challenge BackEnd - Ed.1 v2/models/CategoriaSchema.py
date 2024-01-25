153f8b8bda73d1e99f26c4ea9520a2a7abd5a988
Lucas de Paula Oliveira <loliveirab1030@gmail.com>
2024-01-25 16:55:27 -0300

    desenvolvimento modelo recurso categoria

---
+++ Challenge BackEnd - Ed.1 v2/models/CategoriaSchema.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, validator
+from typing import Optional
+
+class Categoria(BaseModel):
+    DescricaoCategoria: str
+    Cor: Optional[str]
+
+    @validator('Cor')
+    def valida_cor(cls, value):
+        if value.strip() == '':
+            raise ValueError('Você deve inserir a indicação de uma cor no formato hexadecimal (correspondência css)')
+        else:
+            return value.strip()
+    
+    @validator('DescricaoCategoria')
+    def valida_descricao(cls, value):
+        if value.strip() == '':
+            raise ValueError('Você deve inserir uma descrição válida')
+        else:
+            return value.strip().capitalize()

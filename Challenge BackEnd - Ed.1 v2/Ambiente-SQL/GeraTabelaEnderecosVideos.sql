b83012f612cf75c82c02ab81c34d31de4203c11c
Lucas de Paula Oliveira <loliveirab1030@gmail.com>
2024-01-25 15:37:41 -0300

    migracao - pasta atualizacoes banco de dados

---
+++ Challenge BackEnd - Ed.1 v2/Ambiente-SQL/GeraTabelaEnderecosVideos.sql
@@ -0,0 +1,10 @@
+drop table if exists cbe.EnderecosVideos
+go
+
+create table cbe.EnderecosVideos 
+(
+	id int not null identity(1,1)
+	,Titulo varchar(1000)
+	,Descricao varchar(1000) 
+	,Url varchar(1000) not null
+)

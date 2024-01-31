eb64fa401653b95e611574b64224eaab365caba0
Lucas de Paula Oliveira <loliveirab1030@gmail.com>
2024-01-31 19:27:43 -0300

    preparando arquitetura do projeto

---
+++ SQL Alchemy - Essential/03sqla_sync/models/model_base.py
@@ -0,0 +1,5 @@
+# from sqlalchemy.ext.declarative import declarative_base # sqlalchemy version 1.4.31 (abaixo de 2.0)
+from sqlalchemy.orm import declarative_base
+
+ModelBase = declarative_base()
+#print('testando')

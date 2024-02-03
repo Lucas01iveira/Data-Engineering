25e24bc7fceae74cbf8f6f68ab2188f15b6585fa
Lucas de Paula Oliveira <loliveirab1030@gmail.com>
2024-02-03 12:56:53 -0300

    adicao - modelo ingredientes

---
+++ SQL Alchemy - Essential/s03sqla_sync/models/ingredientes.py
@@ -0,0 +1,16 @@
+import sqlalchemy as sa 
+
+import datetime 
+
+from models.model_base import ModelBase
+
+class Ingredientes(ModelBase):
+    __table_name__ = 'ingredientes'
+
+    id: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True, nullable=False)
+    data_criacao: datetime = sa.Column(sa.Datetime, default=datetime.now(), nullable=False, index=True)
+
+    nome: str = sa.Column(sa.String(45), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'<Ingrediente: {self.nome}>'

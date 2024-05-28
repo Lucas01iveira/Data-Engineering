124962022acd20a56241733c1272a47affb52941
Lucas de Paula Oliveira <loliveirab1030@gmail.com>
2024-05-28 20:18:52 -0300

    implementando o método de save na classe de dados

--- Level 1 - Data Engineering/Formation - First Steps as a Data Engineer/Data Pipelines - Combining Python and OOP/pipeline_dados/scripts/processamento_dados.py
+++ Level 1 - Data Engineering/Formation - First Steps as a Data Engineer/Data Pipelines - Combining Python and OOP/pipeline_dados/scripts/processamento_dados.py
@@ -3,26 +3,26 @@ import csv
 
 class Dados:
 
-    def __init__(self, path, tipo_dado):
+    def __init__(self, path: str, tipo_dado: str) -> object:
         self._path = path
         self._tipo_dado = tipo_dado
         self._dados = self.read_file()
         self._nome_colunas = self.get_columns()
     
     # tornando os objetos da classe iteráveis
-    def __getitem__(self, item):
+    def __getitem__(self, item) -> None:
         return self._dados[item]
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._dados)
 
-    def read_json(self):
+    def read_json(self) -> list:
         
         with open(self._path, 'r') as file:
             file_data = json.load(file) 
         return file_data
 
-    def read_csv(self):
+    def read_csv(self) -> list:
         file_data = []
 
         with open(self._path, 'r', encoding= 'utf-8') as file:
@@ -32,20 +32,25 @@ class Dados:
         
         return file_data
 
-    def read_file(self):
+    def read_file(self) -> list:
         if self._tipo_dado.upper() == 'JSON':
             return self.read_json()
         
         elif self._tipo_dado.upper() == 'CSV':
             return self.read_csv()
         
+        elif self._tipo_dado.upper() == 'LIST':
+            dados = self._path
+            self._path = 'lista de registros definida em memória'
+            return dados
+
         else:
             pass
 
-    def get_columns(self):
+    def get_columns(self) -> list:
         return self._dados[-1].keys()
         
-    def rename_columns(self, column_map: dict):
+    def rename_columns(self, column_map: dict) -> object:
         new_file_data = []
         
         # element (type dict)
@@ -61,19 +66,19 @@ class Dados:
         self._nome_colunas = self.get_columns()
 
     @property
-    def path(self):
+    def path(self) -> str:
         return self._path
 
     @property
-    def tipo_dado(self):
+    def tipo_dado(self) -> str:
         return self._tipo_dado
     
     @property
-    def dados(self):
+    def dados(self) -> str:
         return self._dados
     
     @staticmethod
-    def combine_data(data1, data2, column_layout):
+    def combine_data(data1 : object, data2: object, column_layout: dict) -> object:
         total_data = []
         total_data.extend(data1)
         total_data.extend(data2)
@@ -88,7 +93,12 @@ class Dados:
             
             total_data_treated.append(new_dict)
 
-        return total_data_treated
+        return Dados(total_data_treated, 'list')
 
-    
+    @staticmethod
+    def save_file(path: str, related_data: object, column_layout: list) -> None:
+        with open(path, 'w', encoding= 'utf-8') as file:
+            file_writer = csv.DictWriter(file, fieldnames= column_layout)
+            file_writer.writeheader()
+            file_writer.writerows(related_data)
 

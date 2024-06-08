079816963b82dde8c3cadf265f2eb37d25660ede
Lucas de Paula Oliveira <loliveirab1030@gmail.com>
2024-06-08 13:35:09 -0300

    ajsute codigo definicao classe

--- Level 1 - Data Engineering/Formation - First Steps as a Data Engineer/Python and APIs - Knowing the requests module/dados_repos.py
+++ Level 1 - Data Engineering/Formation - First Steps as a Data Engineer/Python and APIs - Knowing the requests module/dados_repos.py
@@ -11,48 +11,48 @@ class DadosRepositorios:
         self.headers = {'Authorization': 'Bearer '+ self.access_token,
             'X-GitHub-Api-Version': '2022-11-28'}
 
-def lista_repositorios(self):
-    repos_list = []
+    def lista_repositorios(self):
+        repos_list = []
 
-    for page_num in range(1, 20):
-        try:
-            url = f'{self.api_base_url}/users/{self.owner}/repos?page={page_num}'
-            response = requests.get(url, headers= self.headers)
-            repos_list.append(response.json())
-        except:
-                repos_list.append(None)
-
-    return repos_list
-
-def nomes_repos(self, repos_list): 
-    repo_names=[] 
-    for page in repos_list:
-        for repo in page:
+        for page_num in range(1, 20):
             try:
-                repo_names.append(repo['name'])
-            except: 
-                    pass
-
-    return repo_names
-
-def nomes_linguagens(self, repos_list):
-    repo_languages=[]
-    for page in repos_list:
-        for repo in page:
-            try:
-                    repo_languages.append(repo['language'])
+                url = f'{self.api_base_url}/users/{self.owner}/repos?page={page_num}'
+                response = requests.get(url, headers= self.headers)
+                repos_list.append(response.json())
             except:
-                    pass
+                    repos_list.append(None)
 
-    return repo_languages
+        return repos_list
 
-def cria_df_linguagens(self):
-    repositorios = self.lista_repositorios()
-    nomes = self.nomes_repos(repositorios)
-    linguagens = self.nomes_linguagens(repositorios)
+    def nomes_repos(self, repos_list): 
+        repo_names=[] 
+        for page in repos_list:
+            for repo in page:
+                try:
+                    repo_names.append(repo['name'])
+                except: 
+                        pass
 
-    dados = pd.DataFrame()
-    dados['repository_name'] = nomes
-    dados ['language'] = linguagens
+        return repo_names
 
-    return dados
+    def nomes_linguagens(self, repos_list):
+        repo_languages=[]
+        for page in repos_list:
+            for repo in page:
+                try:
+                        repo_languages.append(repo['language'])
+                except:
+                        pass
+
+        return repo_languages
+
+    def cria_df_linguagens(self):
+        repositorios = self.lista_repositorios()
+        nomes = self.nomes_repos(repositorios)
+        linguagens = self.nomes_linguagens(repositorios)
+
+        dados = pd.DataFrame()
+        dados['repository_name'] = nomes
+        dados ['language'] = linguagens
+
+        return dados
